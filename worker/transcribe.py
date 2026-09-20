ead6b2225bbca7adf760f1e5fddc647be3e4564a
techstudio-88 <techstudio7808@gmail.com>
2026-09-20 14:12:48 +0530

    Reduce Whisper worker memory footprint

--- worker/transcribe.py
+++ worker/transcribe.py
@@ -2,8 +2,8 @@ import json,sys
 from faster_whisper import WhisperModel
 audio=sys.argv[1]
 model_name=sys.argv[2] if len(sys.argv)>2 else "tiny"
-model=WhisperModel(model_name,device="cpu",compute_type="int8")
-segments,_=model.transcribe(audio,language=None,vad_filter=True,word_timestamps=False)
+model=WhisperModel(model_name,device="cpu",compute_type="int8",cpu_threads=1,num_workers=1)
+segments,_=model.transcribe(audio,language=None,vad_filter=True,word_timestamps=False,beam_size=1,condition_on_previous_text=False)
 out=[]
 for s in segments:
     text=s.text.strip()
